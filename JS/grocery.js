5eff9d59c219c48b6a0639c10ae748035fa049a3
Zeeshan Ansari <zeeshan.ansari@nagarro.com>
2016-07-08 15:55:50 +0530

    First Commit

---
+++ JS/grocery.js
@@ -0,0 +1,22 @@
+(function () {
+    "use strict";
+    // Declare your variables
+    var ulGroceries;
+    // Assign your variable
+    ulGroceries = document.querySelector("somthing");
+    groceryPlaceHolder = "{{grocery}}"
+    // create your functions
+
+    // add your event listeners
+
+    // call Init
+
+    //return something
+
+    //for handling spaces
+    //.trim;
+    //#ulGroceries input[type=radio]:checked
+    //selectedRadaio.parent.parent.remove();
+
+    //window.localStorage.setItem("grocery",JSON.stringify(array[]));
+})();
